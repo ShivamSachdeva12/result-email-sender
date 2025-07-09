2c0a42b56a75bc9ae85d1a36fcb60b170a7c3f5f
ShivamSachdeva12 <sachdevashivam506@gmail.com>
2025-07-10 02:19:44 +0530

    modified

--- app.py
+++ app.py
@@ -203,5 +203,6 @@ def generate():
                            download_link=f"/download/{filename}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env var
+    app.run(host='0.0.0.0', port=port)
 
